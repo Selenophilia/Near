f75a8e6101c31f8b663c96e2c4da2a1ab8992d18
Paulo Argenal <pauloargenal@gmail.com>
2020-04-03 16:17:09 +0800

    Add user exceptions

---
+++ app/Exceptions/UserException.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { LogicalException } = require('@adonisjs/generic-exceptions')
+
+class UserException extends LogicalException {
+    handle (error, { response }) {
+        response
+          .status(500)
+          .send('Custom exception handled!')
+      }
+
+      constructor(){
+          const message = 'User already exists'
+          const status = 400
+          const code = 'BadRequestException'
+          super(message, status, code)
+      } 
+}
+
+module.exports = UserException
